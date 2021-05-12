cb6eff55468e8912b54be05e9aad7a338a75e824
Henry Guo <henry.k.guo@gmail.com>
2021-05-12 04:48:21 -0400

    added form logic and database logic

--- src/Pages/Update.js
+++ src/Pages/Update.js
@@ -2,10 +2,38 @@ import { Fragment } from "react";
 import AddForm from "../components/forms/AddForm";
 
 const Update = () => {
+    async function addWeaponHandler(item) {
+        const url = `https://genshin-data-71857-default-rtdb.firebaseio.com/weapon.json`;
+        console.log(url);
+        const response = await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(item),
+            headers: {
+                'Content-type' : 'application/json'
+            }
+        });
+        const data = await response.json();
+        console.log(data);
+    }
+
+    async function addCharacterHandler(item) {
+        const url = `https://genshin-data-71857-default-rtdb.firebaseio.com/character.json`;
+        console.log(url);
+        const response = await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(item),
+            headers: {
+                'Content-type' : 'application/json'
+            }
+        });
+        const data = await response.json();
+        console.log(data);
+    }
+
   return (
     <Fragment>
-      <AddForm item="Weapon"></AddForm>
-      <AddForm item="Character"></AddForm>
+      <AddForm item="Weapon" onAddItem={addWeaponHandler}></AddForm>
+      <AddForm item="Character" onAddItem={addCharacterHandler}></AddForm>
     </Fragment>
   );
 };
--- src/components/forms/AddForm.js
+++ src/components/forms/AddForm.js
@@ -1,24 +1,51 @@
 import classes from "./AddForm.module.css";
 import Card from "../UI/Card";
+import { useRef } from "react";
 
 const AddForm = (props) => {
+  const descriptionRef = useRef('');
+  const itemRef = useRef('');
+  const imageRef = useRef('');
+  const rarityRef = useRef(0);
+
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+
+    const item = {
+      item: itemRef.current.value,
+      description: descriptionRef.current.value,
+      rarity: rarityRef.current.value,
+      image: imageRef.current.value
+    }
+    props.onAddItem(item);
+    
+    itemRef.current.value = '';
+    descriptionRef.current.value = '';
+    rarityRef.current.value = '';
+    imageRef.current.value = ''; 
+  }
+
   return (
     <Card>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={submitFormHandler}>
         <div className={classes.control}>
           <label htmlFor="item">{props.item}</label>
-          <input id="item" type="text"></input>
+          <input id="item" type="text" ref={itemRef}></input>
         </div>
         <div className={classes.control}>
           <label htmlFor="description">Description</label>
-          <textarea id="description" rows="5"></textarea>
+          <textarea id="description" rows="5" ref={descriptionRef}></textarea>
         </div>
         <div className={classes.control}>
           <label htmlFor="rarity">Rarity</label>
-          <input id="rarity" type="number"></input>
+          <input id="rarity" type="number" ref={rarityRef} min='3' max='5'></input>
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="image">Image Url</label>
+          <input id="image" type="text" ref={imageRef}></input>
         </div>
         <div className={classes.actions}>
-          <button className={classes.btn}>Add {props.item}</button>
+          <button type="submit" className={classes.btn}>Add {props.item}</button>
         </div>
       </form>
     </Card>
